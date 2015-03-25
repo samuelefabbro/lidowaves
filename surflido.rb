312ab088c2ec124842be90229793d751e95b200d
Samuele Fabbro <samu@MacBook-Pro-di-Samuele.local>
2015-03-25 14:37:51 +0100

    views

--- surflido.rb
+++ surflido.rb
@@ -3,13 +3,16 @@ require 'open-uri'
 require 'sinatra'
 
 
+def h_onda
+	doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
+	@h_onda = doc.css('td')[15].inner_html
+	erb :index
+end
 
-
-
+# h_onda_max = doc.css('td')[13].inner_html
 
 get '/' do
-	doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
-	h_onda = doc.css('td')[15].inner_html
-	# h_onda_max = doc.css('td')[13].inner_html
 	h_onda
 end
+
+  
