29aa9a5d8ac0f3272295a1e012f56027bd907d54
Samuele Fabbro <samu@MacBook-Pro-di-Samuele.local>
2015-03-25 14:31:08 +0100

    incluso nel get

--- surflido.rb
+++ surflido.rb
@@ -3,14 +3,13 @@ require 'open-uri'
 require 'sinatra'
 
 
-doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
 
-# search = doc.css('td').children
 
-h_onda_max = doc.css('td')[13].inner_html
-h_onda = doc.css('td')[15].inner_html
 
 
 get '/' do
+	doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
+	h_onda = doc.css('td')[15].inner_html
+	# h_onda_max = doc.css('td')[13].inner_html
 	h_onda
 end
