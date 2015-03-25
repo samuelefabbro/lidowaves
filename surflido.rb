41a63f337ebbe543f9853d8dcba9d60745158585
Samuele Fabbro <samu@MacBook-Pro-di-Samuele.local>
2015-03-25 18:42:48 +0100

    Aggiunti valori

--- surflido.rb
+++ surflido.rb
@@ -3,16 +3,36 @@ require 'open-uri'
 require 'sinatra'
 
 
-def h_onda
-	doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
-	@h_onda = doc.css('td')[15].inner_html
-	erb :index
+def piattaforma
+	@doc = Nokogiri::HTML(open("http://93.62.201.234/reali/reale.php"))
 end
 
-# h_onda_max = doc.css('td')[13].inner_html
+def h_onda 
+	piattaforma
+	@h_onda = @doc.css('td')[15].inner_html
+end
+
+def h_onda_max
+	piattaforma
+	@h_onda_max = @doc.css('td')[13].inner_html
+end
+
+def vento
+	piattaforma
+	@vento = @doc.css('td')[7].inner_html
+end
+
+def dir_vento
+	piattaforma
+	@dir_vento = @doc.css('td')[11].inner_html
+end
 
 get '/' do
 	h_onda
+	h_onda_max
+	vento
+	dir_vento
+	erb :index
 end
 
   
